84591736fbd7d69583d686fabd3a7346f1b3bd90
Sarah <Sarah19921028@gmail.com>
2025-08-28 13:35:27 -0700

    new definitions and scroll bar for dictionary

--- definitions.js
+++ definitions.js
@@ -3,16 +3,22 @@ const dreamInterpretations = {
         { word: 'Abduction', definition: 'a feeling of being trapped, powerless, or held against your will' },
         { word: 'Abyss', definition: 'the unknown or deep fear, can represent the subconscious' },
         { word: 'Accomplice', definition: 'feelings of guilt or complicity in a situation' },
-        { word:  'Adultery', definition: 'reflects feelings of betrayal, guilt, or moral conflict' },
-        { word:  'Advertisement', definition: 'symbolizes a desire for attention, recognition, or validation' },
-        { word:  'Aggression', definition: 'represents repressed anger, frustration, or a need to assert oneself' },
-        { word:  'Aliens', definition: 'feelings of being different, isolated, or misunderstood'     },
-        {word:   'Ambulance ', definition: 'urgent need for help, healing, or rescue in some aspect of life'},
+        { word: 'Adultery', definition: 'reflects feelings of betrayal, guilt, or moral conflict' },
+        { word: 'Advertisement', definition: 'symbolizes a desire for attention, recognition, or validation' },
+        { word: 'Aggression', definition: 'represents repressed anger, frustration, or a need to assert oneself' },
+        { word: 'Aliens', definition: 'feelings of being different, isolated, or misunderstood'     },
+        { word: 'Ambulance ', definition: 'urgent need for help, healing, or rescue in some aspect of life'},
     ],
 
     B: [
-        { word: 'Banana', definition: 'A long, curved fruit with a yellow skin.' },
-        { word: 'Book', definition: 'A written or printed work consisting of pages bound together.' }
+        { word: 'Beach', definition: 'signifies a state of transition or a need for relaxationa and reflection' },
+        { word: 'Bear', definition: 'represents a powerful force like protectiion or raw untamed emotion' },
+        { word: 'Bed', definition: 'symbolizes intimacy and introspection or a need for deep rest' },
+        { word: 'Bell', definition: 'represents a call to attention, awakening or a significant change' },
+        { word: 'Bird', definition: 'symbolizes freedom, perspective and transcendence' },
+        { word: 'Blood', definition: 'represents life force, vitality and deep emotional connections' },
+        { word: 'Book', definition: 'symbolizes knowledge, wisdom and the quest for understanding' },
+        { word: 'Bridge', definition: 'indicates connection, transition and overcoming obstacles' },
     ],
 };
 
